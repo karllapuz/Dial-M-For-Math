16b8d97af3ae0180c9fd6131080cb32f4aeab57e
Karl Lapuz <karllapuz25@gmail.com>
2018-11-25 03:25:52 -0800

    Add dial and script

---
+++ Assets/Scripts/SpinningDial.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningDial : MonoBehaviour {
+
+    int speed = 60;
+    float lastRotation = 0;
+    float rotationSpeed;
+    float direction;
+    bool rotatingLeft = true;
+
+    // Use this for initialization
+    void Start () {
+    
+        transform.Rotate(0, 0, direction);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rotationSpeed = speed * Time.deltaTime;
+        direction = lastRotation + rotationSpeed;
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            lastRotation = transform.rotation.z;
+            if (rotatingLeft) {
+                direction = lastRotation - rotationSpeed;
+            }
+            else {
+                direction = lastRotation + rotationSpeed;
+            }
+            rotatingLeft = !rotatingLeft;
+        }
+        transform.Rotate(0, 0, direction);
+    }
+}
